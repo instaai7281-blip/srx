7c13400a9dc000b68c5bc18032e1845429b97b59
Rocking Star <rockingstar.21rs@gmail.com>
2026-04-11 10:15:38 +0530

    Update default BOT_TOKEN

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ YTUB_COOKIES = """
 
 API_ID = int(getenv("API_ID", "27317669"))
 API_HASH = getenv("API_HASH", "11b88c331c5d44fde57cf91de1a2156b")
-BOT_TOKEN = getenv("BOT_TOKEN", "8415514217:AAFgSxR_RPuzge7J7tXYDFrmIFalRUeFTB8")
+BOT_TOKEN = getenv("BOT_TOKEN", "7939263258:AAHCZgg2Uy3I-U8ZTuZS85BtEYVh9DGh5d4")
 OWNER_ID = list(map(int, getenv("OWNER_ID", "6947378236").split()))
 MONGO_DB = getenv("MONGO_DB", "mongodb+srv://beanonymoushacker09:q9gxQwIZDCmVOycI@cluster0.ej0gk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 LOG_GROUP = getenv("LOG_GROUP", "-1002896498332")
